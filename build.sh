870ff08cb71d1d7a6f8430fa53dcc33a7937a61b
compermane <comper.mane@gmail.com>
2025-07-17 14:50:56 -0300

    ajustes

--- build.sh
+++ build.sh
@@ -1,3 +1,11 @@
+# Pegar as imagens
+eval $(minikube docker-env)
+
+docker build -t gontabilizador-app:latest .
+docker build -t mysql:latest ./db
+docker build -t custom-nginx:lastest ./nginx
+docker build -t python-listener:lasters ./python
+
 # Iniciar o minikube
 minikube start
 
