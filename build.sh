67da40114375cd810b63eb5518091744f6236921
compermane <comper.mane@gmail.com>
2025-07-23 16:30:56 -0300

    correções

--- build.sh
+++ build.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 
+# Iniciar o minikube
+minikube start
+
+# Iniciar o ingress
+minikube addons enable ingress
+
 # Pegar as imagens
 eval $(minikube docker-env)
 
 docker build -t gontabilizador-app:latest .
-docker build -t mysql:latest ./db
-docker build -t custom-nginx:lastest ./nginx
-docker build -t python-listener:lasters ./python
-
-# Iniciar o minikube
-minikube start
+docker build -t custom-mysql:latest ./db
+docker build -t custom-nginx:latest ./nginx
+docker build -t python-listener:latest ./python
 
 helm install gontabilizador ./gontabilizador
